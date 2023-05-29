17bb0ddaf87ba082228e3cd7d90c09e11898df3d
miya <sirotumesirokuma@gmail.com>
2023-05-29 21:30:43 +0900

    init commit

--- __tests__/NavBar.test.tsx
+++ __tests__/NavBar.test.tsx
@@ -9,6 +9,8 @@ import { setupServer } from 'msw/node'
 import { handlers } from '../mock/handlers'
 import 'setimmediate'
 
+process.env.NEXT_PUBLIC_HASURA_URL = 'https://strong-firefly-45.hasura.app/v1/graphql'
+
 initTestHelpers()
 
 const server = setupServer(...handlers)
